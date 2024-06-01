1cd2f1e438fdcfbe479fa48a1375be4add6d8175
Umar Togo <Shaxriyortolibov889@gmail.com>
2024-06-01 16:40:59 +0500

    add readme

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="container">
+      <div className="calculator">
+        <div className="formInput">
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+        </div>
+        <div>
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+        </div>
+        <div>
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+        </div>
+        <div>
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+        </div>
+        <div>
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+          <input type="button" value="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
